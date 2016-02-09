ae81330e7b754c9df6bf39149ba65ecd437b431b
Paul Totzke <totzke.paul@gmail.com>
2016-02-08 23:37:01 -0600

    Initial Commit

---
+++ Peace/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Peace
+//
+//  Created by Paul Totzke on 2/8/16.
+//  Copyright © 2016 Paul Totzke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
